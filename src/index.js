488928d2b5702653f05c39ad5168a13ba25906be
zZ <z@zZ.local>
2016-05-24 22:55:52 +0800

    add file upload

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ export default class Library {
     }
     start(data) {
         var file;
+        var reader = new FileReader();
 
         this._el = document.getElementById(data.el);
 
@@ -22,8 +23,60 @@ export default class Library {
 
         file = this._el.files[0];
 
-        console.log(file);
+        reader.onloadstart = function () {
+            console.log('onloadstart');
+        };
 
+        reader.onprogress = function () {
+            console.log('onprogress');
+        };
+
+        reader.onload = function () {
+            console.log('onload');
+        };
+
+        reader.onloadend = function () {
+            var xhr,
+                fileData = new FormData(),
+                nCountNum = 0,
+                nSliceCount = 100,
+                nFactSize = file.size / nSliceCount,
+                nFactCount;
+
+            var start = nCountNum * nFactSize,
+                end = Math.min(start + nFactSize, file.size);
+
+            fileData.append('file', file.slice(start, end));
+            fileData.append('name', file.name);
+            fileData.append('size', file.size);
+            fileData.append('type', file.type);
+            fileData.append('totalCount', nFactCount);
+            fileData.append('indexCount', nCountNum);
+            fileData.append('totalCount', 1);
+            fileData.append('indexCount', 1);
+            fileData.append('trueName', file.name.substring(0, file.name.lastIndexOf('.')));
+
+//            console.log(file);
+//            console.log(fileData);
+            if (reader.error) {
+                console.log(reader.error);
+            } else {
+                xhr = new XMLHttpRequest();
+                xhr.open('POST', '../php/index.php', true);
+                xhr.overrideMimeType('application/octet-stream');
+                xhr.send(fileData);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            console.log('upload complete');
+                            console.log('response: ' + xhr.responseText);
+                        };
+                    };
+                };
+            }
+        };
+
+        reader.readAsBinaryString(file);
     }
     clear() {
         var self = this;
